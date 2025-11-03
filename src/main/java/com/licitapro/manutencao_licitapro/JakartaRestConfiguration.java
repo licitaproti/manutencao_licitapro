8608384cb0eb374b0f65eabd5d7db10bf100565a
licitaproti <licitaproti@gmail.com>
2025-11-03 07:32:41 -0300

    init page

---
+++ src/main/java/com/licitapro/manutencao_licitapro/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.licitapro.manutencao_licitapro;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/licitapro/manutencao_licitapro/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.licitapro.manutencao_licitapro.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
